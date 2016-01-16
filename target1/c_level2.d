26ed5730ecd811d922f65382b0c1b5f46106b0de
Ning Wang <glningwang@gmail.com>
2016-01-16 21:47:09 +0000

    update attack lab

--- target1/c_level2.d
+++ target1/c_level2.d
@@ -1,9 +1,9 @@
 
-c_level3.o:     file format elf64-x86-64
+c_level2.o:     file format elf64-x86-64
 
 
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 c7 b0 dc 61 55 	mov    $0x5561dcb0,%rdi
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
    7:	c3                   	retq   
